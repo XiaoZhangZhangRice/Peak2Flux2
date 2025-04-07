199175cb737edfd3f77c5b62c78caea1cc66c61e
echeverriaseba <secheverriap@gmail.com>
2025-04-07 16:45:57 +0200

    acc_function

--- R_test/ppm2flux_acc_GWP.R
+++ R_test/ppm2flux_acc_GWP.R
@@ -11,6 +11,9 @@ flux2acc <- function(data) {
 
   Gases <- c("CH4", "N2O", "CO2", "Gas1", "Gas2", "Gas3")
 
+  data_name <- deparse(substitute(data))
+  output_name <- paste0("acc_", data_name) # to call resulting cumulative emissions data frame according to input data frame
+
   flux_df <- data %>%
     mutate(Date =as.Date(Date, format = "%d-%b-%y"))
 
@@ -18,25 +21,43 @@ flux2acc <- function(data) {
   end_date <- max(flux_df$Date)
   date_seq <- seq.Date(start_date, end_date, by = "day")
 
+  flux_df <- flux_df %>% # fill missing dates in between sampling events
+    group_by(Plot) %>%
+    complete(Date = date_seq) %>%
+    ungroup()
+
     ## 1.1. Flux interpolation ####
+  kept_gases <- c()
+  kept_emissions <- c()
 
   for(gas in Gases) {
     flux_col <- paste0(gas, "_flux_corrected")
-
-      flux_df <- flux_df %>%
-          group_by(Plot) %>%
-          complete(Date = date_seq) %>%
-          ungroup()
+    emission_col <- paste0(gas, "_emission_kg_ha")
 
       if (flux_col %in% names(flux_df)) {
         flux_df <- flux_df %>%
-          mutate("{flux_col}" := na.approx(.data[[flux_col]], rule = 2)) %>%
+          mutate("{flux_col}" := na.approx(.data[[flux_col]], rule = 2)) %>% # linear extrapolation for fluxes in between sampling events
+          mutate("{emission_col}" := .data[[flux_col]] * 0.24) %>% # from mg m-2 h-1 to kg ha-1 day-1
           ungroup()
 
-    return(flux_df)
+        kept_gases <- c(kept_gases, flux_col)
+        kept_emissions <- c(kept_emissions, emission_col)
 
   } # closes if() for flux_col %in% names(flux_df)
   } # closes for(gas in Gases)
+
+  cumulative_emissions <- flux_df %>%
+    group_by(Plot) %>%
+    summarise(across(all_of(kept_emissions), ~ sum(.x, na.rm = TRUE))) %>%
+    ungroup()
+
+  assign(output_name, cumulative_emissions, envir = .GlobalEnv)
+
+  flux_df <- flux_df %>%
+    select(Date, Plot, all_of(kept_gases), all_of(kept_emissions))
+
+  return(flux_df)
+
   } # closes f(x)
 
 # 2. Tests ####
